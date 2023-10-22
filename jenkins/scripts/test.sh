9f7f64445b77703cad90163b103ae8f97f261c67
Datalow <63528217+datalow-del@users.noreply.github.com>
2023-10-22 20:11:31 +0300

    Update test.sh

--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -1,20 +1,11 @@
 #!/usr/bin/env sh
 
-echo 'The following "npm" command (if executed) installs the "cross-env"'
-echo 'dependency into the local "node_modules" directory, which will ultimately'
-echo 'be stored in the Jenkins home directory. As described in'
-echo 'https://docs.npmjs.com/cli/install, the "--save-dev" flag causes the'
-echo '"cross-env" dependency to be installed as "devDependencies". For the'
-echo 'purposes of this tutorial, this flag is not important. However, when'
-echo 'installing this dependency, it would typically be done so using this'
-echo 'flag. For a comprehensive explanation about "devDependencies", see'
-echo 'https://stackoverflow.com/questions/18875674/whats-the-difference-between-dependencies-devdependencies-and-peerdependencies.'
+echo 'Installing any necessary dependencies...'
 set -x
-# npm install --save-dev cross-env
+# Add your installation commands here, e.g., "npm install" or any other package manager you're using
 set +x
 
-echo 'The following "npm" command tests that your simple Node.js/React'
-echo 'application renders satisfactorily. This command actually invokes the test'
-echo 'runner Jest (https://facebook.github.io/jest/).'
+echo 'Testing your Node.js application...'
 set -x
-npm test
+# Add your testing command here, e.g., "npm test"
+set +x
