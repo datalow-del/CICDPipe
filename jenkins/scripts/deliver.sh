8af00351f743c4755c65b67d930638a3f3322afa
Datalow <63528217+datalow-del@users.noreply.github.com>
2023-10-22 20:29:07 +0300

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -2,20 +2,13 @@
 
 # This script assumes you have a Node.js application without React
 
-echo 'Building your Node.js application...'
-set -x
-# Assuming your build command is something like "npm run build"
-npm run build
-set +x
-
 echo 'Starting your Node.js application...'
 set -x
-# Assuming your start command is something like "node app.js"
-node app.js &
+npm start &
 # Add any necessary startup parameters or environment variables here
 sleep 1
 echo $! > .pidfile
 set +x
 
 echo 'Your Node.js application is now running.'
-echo 'Visit http://localhost:3000 to interact with it.'
+
