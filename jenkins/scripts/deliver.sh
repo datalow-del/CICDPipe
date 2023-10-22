a75d2f762c9d84da933f0f866d0954e25af087a2
Mohamed Adil <xgamermod76@gmail.com>
2023-10-22 17:49:40 +0000

    1

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -4,15 +4,10 @@
 
 echo 'Starting your Node.js application...'
 set -x
-npm start &
+node app.js &
 # Add any necessary startup parameters or environment variables here
 sleep 1
 echo $! > .pidfile
 set +x
 
-<<<<<<< HEAD
 echo 'Your Node.js application is now running.'
-=======
-echo 'Your Node.js application is now running.'
-
->>>>>>> 137d2e37afb8ec955930ba50b47127a78a549d6f
